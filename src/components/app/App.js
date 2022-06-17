a408b0507cefa3faa3ccedb0a8539f8e2cca5be1
natacha <nat_bartu@hotmail.com>
2022-06-17 10:52:36 +1000

    import Cards components into App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+
+import Cards from "../cards/Cards";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>App</h1>
+      <Cards />
     </div>
   );
 }
