4c594acd7327db1c1c14807436c63275a234b40a
natacha <nat_bartu@hotmail.com>
2022-06-17 12:43:17 +1000

    add dependencies to fix the error from

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,7 +1,19 @@
 import React from "react";
+import boys from "./../../assets/boys.jpeg";
+
+import { Typography, Card, Container, CardMedia } from "@mui/material";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <Container>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" heigth="80vh" src={boys} alt="boys" />
+        <Typography gutterBottom variant="h3" component="div">
+          Boys
+        </Typography>
+      </Card>
+    </Container>
+  );
 };
 
 export default Cards;
